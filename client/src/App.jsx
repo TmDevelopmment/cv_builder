1c3bb14820bbae29b43b338a3b8b10feeb31f2ed
TmDevelopment <tmgamig69@gmail.com>
2026-05-21 15:10:45 +0530

    Restore auth on load and add Toaster
    On mount, App now attempts to restore the authenticated user by reading a token from localStorage and calling /api/users/data; if a user is returned it dispatches an auth/login action and toggles loading state via setLoading. Also adds react-hot-toast Toaster to the app root and includes minor code formatting/quote consistency changes. Errors are logged and loading is cleared on failure.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,61 @@
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import ResumeBuilder from './pages/ResumeBuilder'
-import Preview from './pages/Preview'
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
-import Layout from './pages/Layout'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import ResumeBuilder from "./pages/ResumeBuilder";
+import Preview from "./pages/Preview";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import Layout from "./pages/Layout";
+import { useDispatch } from "react-redux";
+import { setLoading } from "./app/features/authSlice";
+import api from "../configs/api";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const getUserData = async () => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    try {
+      if (token) {
+        const { data } = await api.get("/api/users/data", {
+          headers: {
+            Authorization: token,
+          },
+        });
+        if (data.user) {
+          dispatch({ type: "auth/login", payload: { user: data.user, token } });
+        }
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    } catch (error) {
+      dispatch(setLoading(false));
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='app' element={<Layout />}>
-        <Route index element={<Dashboard />} />
-        <Route path='builder/:resumeId' element={<ResumeBuilder />} />
-      </Route>
-      <Route path='view/:resumeId' element={<Preview />} />
-      <Route path='login' element={<Login />} />
-      <Route path='signup' element={<SignUp />} />
-    </Routes>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="app" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="builder/:resumeId" element={<ResumeBuilder />} />
+        </Route>
+        <Route path="view/:resumeId" element={<Preview />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
