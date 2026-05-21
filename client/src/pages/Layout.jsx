21079cad777293674f248e8f89266c857c6c6cb6
TmDevelopment <tmgamig69@gmail.com>
2026-05-21 15:12:06 +0530

    Add auth-based rendering and loader to Layout
    Use Redux auth state in Layout.jsx to conditionally render app content: show Loader while auth is loading, render main app (Navbar + Outlet) when a user exists, and show the Login page when not authenticated. Also added a console.log for debugging and adjusted the container background className.

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,13 +1,26 @@
 import { Outlet } from "react-router-dom";
 import Navebar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import Loader from "../components/Loader";
+import Login from "./Login";
 
 const Layout = () => {
+  const { user, loading } = useSelector((state) => state.auth);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div>
-      <div className="min-h-screen bg-gradient-to-b from-green-50 to-white">
-        <Navebar />
-        <Outlet />
-      </div>
+      {user ? (
+        <div className="min-h-screen bg-linear-to-b from-green-50 to-white">
+          <Navebar />
+          <Outlet />
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 };
